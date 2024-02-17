bc80a07623e950200a6a3cbcd94ac085d1b69dc6
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-02-17 17:51:53 -0600

    build: init vite react template with personal set up

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {browser: true, es2020: true},
+  extends: [
+    "prettier",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: {ecmaVersion: "latest", sourceType: "module"},
+  settings: {react: {version: "18.2"}},
+  plugins: ["react-refresh", "react"],
+  rules: {
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": ["warn", {allowConstantExport: true}],
+    "indent": ["error", 2],
+    "linebreak-style": 0, //
+    "quotes": ["error", "double"],
+    "semi": ["error", "always"],
+    "no-unused-vars": "off",
+  },
+};
